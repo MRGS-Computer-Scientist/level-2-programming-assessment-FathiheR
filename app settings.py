dd6289ae2945faaf57079e1303c3626b433764b9
FathiheR <21227@students.mrgs.school.nzR>
2024-05-24 12:34:13 +1200

    i tryed to make buttons then it dident work out so i whe back to this

---
+++ app settings.py
@@ -0,0 +1,4 @@
+w_width = 500
+w_height = 500
+
+app_title ="my app_title"
--- main.py
+++ main.py
@@ -1,28 +1,18 @@
-import tkinter as tk
-import random
-word_list = [
-{
-    "word": "Word 1",
-"meaning": "Meaning of Word 1"
-"word": "Word 2",
-"meaning": "Meaning of Word 2"
-},
-{
-"word": "Word 3",
-"meaning": "Meaning of Word 3"
-},
-]
-
-window = tk.Tk()
+from tkinter import *
+window = Tk()
 window.geometry("300x600")
-window.title("Pride Conversation")
-
-word_to_display = random.choice (word_list)
-question_label = tk. Label (wrap=200, text="Do you know the words used in the rainbow community?")
+window.title("Students tracker App")
+question_label = Label(text="student tracker")
 question_label.pack()
-word_label = tk.Label (text= word_to_display ["word"])
+
+word_label = Tk.Label(text="(WORD)")
 word_label.pack()
-yes_button = tk. Button (text="I know this!")
-yes_button.pack()
-no_button = tk.Button (text="Not sure.")
-no_button.pack()
+
+yes_button = Tk.Button(WORD_screen, text="student tracker")
+yes_buttom.pack()
+
+no_button = Tk.Button(WORD_screen, text="quit")
+no_buttom.pack()
+
+window.mainloop()
+ 
