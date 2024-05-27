afe0baaecfd3dc695fbf66a60f5d0c496b09b48e
FathiheR <21227@students.mrgs.school.nzR>
2024-05-28 09:53:51 +1200

    i change the background colour to blue

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
+bg_color="#4895c2"
+
+# "Red", "Blue", "Greem"
+
+
 from tkinter import *
 window = Tk()
 window.geometry("300x600")
+window.configure(bg=bg_color)
 window.title("Students tracker App")
 question_label = Label(text="student tracker")
 question_label.pack()
 
-btn = Button(window, text = 'EXIT !', 
-                command = window.destroy) 
 
+
+btn = Button(window, text = 'EXIT !', command = window.destroy) 
 btn.pack(side = 'top')  
 
-btn = Button(window, text = 'continue !', 
-                command = window.destroy)
-
+btn = Button(window, text = 'continue !', command = window.destroy)
 btn.pack(side = 'top')  
-
 
 window.mainloop()
