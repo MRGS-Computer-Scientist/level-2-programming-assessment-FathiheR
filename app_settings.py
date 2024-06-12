7734f78abd746eebea231a4c58f61905933e135d
FathiheR <21227@students.mrgs.school.nzR>
2024-06-12 14:39:25 +1200

    i made a table for names and how many times students left class

--- app_settings.py
+++ app_settings.py
@@ -1,4 +1,6 @@
 w_width = 500
 w_height = 500
 
+bg_color="#4895c2"
+
 app_title ="my app_title"
--- main.py
+++ main.py
@@ -1,27 +1,35 @@
-bg_color="#4895c2"
-
-# "Red", "Blue", "Greem"
-
-
+from app_settings import *
 from tkinter import *
+
 window = Tk()
 window.geometry("800x800")
 window.configure(bg=bg_color)
 window.title("Students tracker App")
-question_label = Label(text="student tracker")
-question_label.pack()
 
+question_label = Label(text="Student Tracker")
+question_label.grid(row=0, column=0, columnspan=2)
 
+btn = Button(window, text = 'Continue !', command = window.destroy)
+btn.grid(row=2, column=0, columnspan=2)
 
-btn = Button(window, text = 'EXIT !', command = window.destroy) 
-btn.pack(side = 'top')  
+btn = Button(window, text = 'EXIT !', command = window) 
+btn.grid(row=1, column=0, columnspan=2)  
 
-btn = Button(window, text = 'continue !', command = window.destroy)
-btn.pack(side = 'top')  
-
-from tkinter import *
 newwindow = Tk()
 newwindow.geometry("800x800")
 
+
+
+
+student_list = Listbox(newwindow)
+student_list.grid(row=3, column = 0)
+
+away_list = Listbox(newwindow)
+away_list.grid(row=3, column = 1)
+
+student_list.insert(0, "Tom")
+away_list.insert(0, "10")
+student_list.insert(0, "Alex")
+away_list.insert(0, "5")
 
 window.mainloop()
